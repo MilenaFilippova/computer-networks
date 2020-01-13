f6de3d24bd04551d36618bb7f230b8273ce40da6
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2020-01-13 20:38:22 +0800

    Update app_cable_diagnostic.py

--- app_cable_diagnostic.py
+++ app_cable_diagnostic.py
@@ -7,10 +7,21 @@
 from telnetlib import Telnet
 #https://docs.python.org/3/library/telnetlib.html для работы с консолью
 
+#IP='10.90.90.29'
+#port='1-5'
+
 IP = input("For analistic enter your IP: ")
-#вводим порт  длину кабеля в котором нужно измерить
-port =(int)( input('Enter port : '))
 telnet = Telnet(IP)
+#вводим порт  длину кабеля в котором нужно измерить
+port = input('Enter port : ')
+
+#имя пользователя и пароль
+telnet.read_until(b'UserName')
+telnet.write(b'\n')
+telnet.read_until(b'PassWord')
+telnet.write(b'\n')
+#пропускаем ненужные строки
+telnet.read_until(b'#')
 
 
 #формируем команду для консоли cable_diag ports номер_порта
@@ -18,6 +29,7 @@ mycommand = 'cable_diag ports ' + str(port) + '\n'
 encod_mycommand = mycommand.encode(encoding = 'ascii',errors = 'strict')
 #print ("Кодированная строка: ", encod_mycommand)
 telnet.write(encod_mycommand)
+print(encod_mycommand)
 
 
 
@@ -25,17 +37,26 @@ result = telnet.read_until(b'DES')
 # print(result.decode('utf-8'))
 for res in result.decode('utf-8').split('\n'):
     #print ("Декодированная строка(result.decode): ", res)
-#    Port   Type      Link Status          Test Result          Cable Length (M)
-    strig = res.split()
-    if strig[3] == 'OK':
-            print("Приблизительная длина кабеля :", str(strig[4]))
-    elif strig[3] == 'Short in Cable':
-            print("Короткое замыкание в кабеле")
-    elif strig[3] == 'Open in Cable': 
-            print("Кабель, подключенный к порту коммутатора, поврежден или не подключен на другой стороне")
-    elif strig[3] == 'Mismatched':
+    # аргументы результ. строки:
+    #   Port   Type      Link Status          Test Result          Cable Length (M)
+    #   делим строку по пробеллам(на слова) 
+    string = res.split()
+    if len(string)==6:
+        if string[4] == 'OK':
+            print("Приблизительная длина кабеля :", str(string[5]))
+        elif string[4] == 'Mismatched':
             print("	Возникли ошибки при диагностике кабеля, требуется перезапустить тест на том же порту")
-    elif strig[3] == 'Line Driver':
-            print("К данному порту не подключён кабель")
-    elif strig[3] == 'Line Driver':
+        else:
+            print("Кабель не работает")
+    elif len(string)==7:    
+        if string[4] == 'Line'  and  string[5] == 'Driver':
             print("Обнаружено высокое электрическое сопротивление на другой стороне кабеля")
+        else:
+            print("Кабель не работает")
+    elif len(string)==8:        
+        if string[4]=='Short'  and  string[5]=='in' and string[6]=='Cable' :
+            print("Короткое замыкание в кабеле")
+        elif string[4]=='Open'  and  string[5]=='in' and string[6]=='Cable' :
+            print("Кабель, подключенный к порту коммутатора, поврежден или не подключен на другой стороне")
+    else:
+        print("Error!")
